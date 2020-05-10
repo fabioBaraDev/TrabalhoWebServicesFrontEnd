76cf1801f5068316ea5c1fcdbad0d8c76befc70b
FABIO BARA <Fabio.Bara@midway.com.br>
2020-05-10 02:58:31 -0300

    Recuperacao de Dados de Endereço

--- ControleDeAlunos/app/src/main/java/br/com/fiap/controledealunos/model/Aluno.kt
+++ ControleDeAlunos/app/src/main/java/br/com/fiap/controledealunos/model/Aluno.kt
@@ -4,9 +4,9 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class Aluno(
     @SerializedName("nome") val nome : String,
     @SerializedName("numeroCartao") val numeroCartao : String,
-    @SerializedName("id") val id : String
-): Parcelable
+    @SerializedName("id") val id : String,
+    @SerializedName("endereco") val endereco : Endereco
+)
---
+++ ControleDeAlunos/app/src/main/java/br/com/fiap/controledealunos/model/Endereco.kt
@@ -0,0 +1,8 @@
+package br.com.fiap.controledealunos.model
+
+data class Endereco(
+    val logradouro: String,
+    val numero: String,
+    val complemento: String,
+    val cep: String
+)
